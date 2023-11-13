f52ce959389902519f69f0a4cbd53ccee4fd529f
Hanze Meng <2254257145@qq.com>
2023-11-12 20:01:51 -0500

    Added unsupported http methods

--- server/server.py
+++ server/server.py
@@ -17,6 +17,8 @@ class CustomRequestHandler(http.server.SimpleHTTPRequestHandler):
     RATE_LIMIT_PERIOD = 60
     RATE_LIMIT_REQUESTS = 5
 
+    MAX_GET_URL_LENGTH = 1024
+
     def __init__(self, *args, **kwargs):
         self.client_last_request_time = {}
         super().__init__(*args, **kwargs)
@@ -34,6 +36,16 @@ class CustomRequestHandler(http.server.SimpleHTTPRequestHandler):
             return True
 
     def do_GET(self):
+        client_address = self.client_address[0]
+        if not self.can_process_request(client_address):
+            self.send_response(429)  # HTTP 429 Too Many Requests
+            self.end_headers()
+            self.wfile.write(b"Rate limit exceeded. Please wait and try again.")
+            return
+        if len(self.path) > self.MAX_GET_URL_LENGTH:
+            self.send_error(414, "URI Too Long")
+            self.end_headers()
+            return
         path = '/' if self.path == '/' else str(self.path).lstrip('/').rstrip('/')
         legal = (path == '/')
         if not legal:
@@ -43,17 +55,11 @@ class CustomRequestHandler(http.server.SimpleHTTPRequestHandler):
                     break
         if not legal:
             self.send_error(403, 'Unauthorized Access')
+            self.end_headers()
         else:
-            client_address = self.client_address[0]
-            if self.can_process_request(client_address):
-                super().do_GET()
-            else:
-                self.send_response(429)  # HTTP 429 Too Many Requests
-                self.end_headers()
-                self.wfile.write(b"Rate limit exceeded. Please wait and try again.")
+            super().do_GET()
 
     def do_POST(self):
-        # logging.info("POST request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
         client_address = self.client_address[0]
         if self.can_process_request(client_address):
             if self.path == '/upload':
@@ -66,7 +72,6 @@ class CustomRequestHandler(http.server.SimpleHTTPRequestHandler):
             self.wfile.write(b"Rate limit exceeded. Please wait and try again.")
 
     def handle_post(self):
-        # logging.info("POST request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length).decode('utf-8')
         # Handle POST data as needed
@@ -107,6 +112,12 @@ class CustomRequestHandler(http.server.SimpleHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b"Bad Request: Invalid content type for file upload.")
 
+    # disable some http methods which will not be implemented for this project
+    def do_PUT(self):
+        self.send_error(405)
+
+    def do_DELETE(self):
+        self.send_error(405)
 
 class ThreadedHTTPServer(socketserver.ThreadingMixIn, http.server.HTTPServer):
     daemon_threads = True
